2d1445488a3f655e5d35833b25372e852093361c
Szymon Świerzyński <s33102@pjwstk.edu.pl>
2026-03-16 23:11:31 +0100

    Changed variable name to course

--- APBD_Lab02/APBD_Lab02/Program.cs
+++ APBD_Lab02/APBD_Lab02/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-String text = "APBD";
-Console.WriteLine("Hello, " + text);
+String course = "APBD";
+Console.WriteLine("Hello, " + course);
+
